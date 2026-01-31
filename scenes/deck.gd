8eb1d8f6df83bebba1ccef9dd899886d367621b8
Lassi Chen <lachen@utu.fi>
2026-01-31 19:00:46 +0200

    Added Deck shuffle and shuffle animation with tweens

--- scenes/deck.gd
+++ scenes/deck.gd
@@ -4,6 +4,7 @@ signal pop_card
 var card_scene = preload("res://scenes/card.tscn")
 var back_sprite = "firemon.svg"
 var deck = []
+var rotate: bool
 
 func _ready():
 	$Sprite.texture = load("res://graphics/cards/back/" + back_sprite)
@@ -11,10 +12,20 @@ func _ready():
 	for i in range(1, 14):
 		for suit in suits:
 			deck.append([i, suit])
+		
 
 func _on_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.pressed:
-		if deck != []:
+		if event.button_index == 3:
+			deck.shuffle()
+			shuffle_animation()
+		elif deck != []:
 			pop_card.emit(self, deck.pop_front())
 		if deck == []:
 			queue_free()
+
+func shuffle_animation():
+	var tween = create_tween()
+	tween.tween_property(self, "rotation", 0.5, 0.13)
+	tween.tween_property(self, "rotation", -0.5, 0.26)
+	tween.tween_property(self, "rotation", 0, 0.13)
