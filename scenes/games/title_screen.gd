b5b37366be7eaed4403cc1e85c5d1b8ea934e09b
Lassi Chen <lachen@utu.fi>
2026-07-24 21:28:33 +0300

    Added a simple title screen for game navigation

---
+++ scenes/games/title_screen.gd
@@ -0,0 +1,23 @@
+extends CanvasLayer
+
+
+func _on_play_button_pressed():
+	$FirstMenu.visible = false
+	$GameMenu.visible = true
+
+
+func _on_back_button_pressed():
+	$FirstMenu.visible = true
+	$GameMenu.visible = false
+
+
+func _on_game_button_1_pressed():
+	get_tree().change_scene_to_file("res://scenes/games/best_of_three.tscn")
+
+
+func _on_game_button_2_pressed():
+	get_tree().change_scene_to_file("res://scenes/games/poker.tscn")
+
+
+func _on_game_button_3_pressed():
+	get_tree().change_scene_to_file("res://scenes/games/sandbox.tscn")
