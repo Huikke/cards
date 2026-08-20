24ab0775c99252173c3a427792ea0c48d254666b
Lassi Chen <lassichen@gmail.com>
2026-08-20 06:22:28 +0300

    Added setting to change deck in EscMenu (only affects new deck instances)

--- scenes/user_interface/esc_menu.gd
+++ scenes/user_interface/esc_menu.gd
@@ -4,6 +4,7 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
 		if visible == true:
 			visible = false
+			_on_back_button_pressed()
 		else:
 			visible = true
 
@@ -12,3 +13,13 @@ func _on_continue_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/games/title_screen.tscn")
+
+
+func _on_settings_pressed() -> void:
+	$FirstMenu.visible = false
+	$SettingsMenu.visible = true
+
+
+func _on_back_button_pressed() -> void:
+	$FirstMenu.visible = true
+	$SettingsMenu.visible = false
