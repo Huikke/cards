41294f3d830d545b1378241c3f48cdaf66c20b1f
Lassi Chen <lassichen@gmail.com>
2026-09-08 07:35:41 +0300

    Created CountArea for Japanese game

---
+++ scenes/objects/count_area.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+var count: int = 0
+
+func _on_area_entered(_area: Area2D) -> void:
+	count += 1
+	update_label()
+
+
+func _on_area_exited(_area: Area2D) -> void:
+	count -= 1
+	update_label()
+
+func update_label():
+	print(count)
+	$Label.text = "Count: " + str(count)
